7ebd36fce255696dd7cf8c5b26053327f56f6aa8
Sherri Li <32345806+shli17@users.noreply.github.com>
2018-12-04 01:07:24 -0800

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -1,24 +1,25 @@
+# Tokenize words, clean data, remove noun words with no sentiment using NLTK's word packages.
+# Save changes to data for each step - tokenize, stem, remove stopwords - into txt files, to see differences with each cleaning.
+# Check the develop branch for comparisons between txt files.
+
 # import nltk
 # nltk.download('stopwords')
 # nltk.download('punkt')
 # nltk.download('words')
 
-# We clean data and save each step into a text file so that we can see the differences with each cleaning.
 import pandas as pd
 import re
 
 from nltk.tokenize import RegexpTokenizer as rt
 from nltk.corpus import stopwords
 from nltk.corpus import words
-# from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 
 import pickle as rick
 
-# read in the csv into a dataframe object
-
+# Read in the csv into a dataframe object
 data = pd.read_csv("train.csv") # saves into a DataFrame object
 phrases = data["Phrase"]
 labels = data["Sentiment"]
@@ -49,7 +50,7 @@ for phrase in phrases:
 #         f.write("%s\n" % item)
 # f.close()
 
-# Initialize lemmatizer, a "stemmer" to turn [cat, cats, catted] -> [cat]
+# Initialize lemmatizer, which stems words.
 # stemmer = WordNetLemmatizer()
 # stemmed = []
 # for phrase in cleaned:
@@ -63,16 +64,16 @@ for phrase in phrases:
 #         f.write("%s\n" % item)
 # f.close()
 
-# Initialize a Stopwords list, built in to nltk. Filter out articles, conjunctions, prepositions.
+# Initialize a Stopwords list, imported from nltk. Remove articles, conjunctions, prepositions.
 # stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 stop_words = list(stopwords.words('english'))
-# add stuff that we observe doesn't get filtered out
+# Extra words to filter out:
 custom_words = ['genre', 'film', 'movie']
 stop_words = set(stop_words).union(set(custom_words))
-# Initialize a list of proper nouns, given in the nltk Words package. Filter out proper nouns.
+
 good_words = set(list(words.words()))
 
-# First cleaning to compare result with second cleaning
+# First cleaning to only remove stopwords
 # result = []
 # for phrase in cleaned:
 #     temp = []
@@ -86,7 +87,7 @@ good_words = set(list(words.words()))
 #         f.write("%s\n" % item)
 # f.close()
 
-# Second cleaning
+# Second cleaning to remove words not in goodwords
 result = []
 for phrase in cleaned:
     temp = []
@@ -104,7 +105,7 @@ print(result)
 # unix $ diff firstclean.txt secondclean.txt
 
 
-# functions
+# Create pickle file
 def generate_cleaned_file(instances, labels):
     col = ["Phrases", "Sentiment"]
     i = range(len((instances)))
@@ -115,30 +116,16 @@ def generate_cleaned_file(instances, labels):
     rick.dump(dataframe, pkfile)
     pkfile.close()
 
+# Read pickle file
 def open_cleaned_file(fileName):
     pkl = open(fileName, 'rb')
     result = rick.load(pkl)
     pkl.close()
     return result
 
-# Generate the pickle file
+# Print contents of the pickle file
 generate_cleaned_file(result, labels)
 dataset = open_cleaned_file('preprocessed_train_data.pkl')
 print(type(dataset))
 print(dataset)
 
-
-# def read_data(fileName):
-#     d = pd.read_csv(fileName)
-#     return d
-#
-# def clean_punct(dataset):
-#     d = re.sub('[^A-Za-z]', '', dataset)
-#     return d
-#
-# def clean_caps(dataset):
-#     d = dataset.lower()
-#     return d
-#
-# def clean_stem(dataset):
-#     stemmer = PorterStemmer()
