b35d79f2bcabe5cadf195282dc4c26919c040c4a
Sherri Li <shli17@users.noreply.github.com>
2018-12-07 20:49:14 -0800

    update README

--- training_testing_LR_NB.py
+++ training_testing_LR_NB.py
@@ -16,13 +16,15 @@ vect = CountVectorizer(min_df=2, ngram_range=(0, 30))
 X_train = vect.fit(result).transform(result)
 
 # Train Logistic Regression model
-param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
+#param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
+param_grid = {'C': [1]}# 1 turned out best
 grid = GridSearchCV(LogisticRegression(), param_grid, cv=5)
 grid.fit(X_train, labels)
 print("Best cross-validation score for LogisticRegression: {:.2f}".format(grid.best_score_))
 
 # Train Naive Bayes model
-param_grid = {'alpha': [0.001, 0.01, 0.1, 1,10],"fit_prior":[True, False]}
+# param_grid = {'alpha': [0.001, 0.01, 0.1, 1, 10, 100],"fit_prior":[True, False]}
+param_grid = {'alpha': [1]}# 1 turned out best
 gs_clf = GridSearchCV(MultinomialNB(),param_grid, cv=5)
 gs_clf.fit(X_train, labels)
 print("Best cross-validation score for MultinomialNB: {:.2f}".format(gs_clf.best_score_))
